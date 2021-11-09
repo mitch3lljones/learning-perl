408269a8ba1c07bccc9d158e4aebac0ba3742b86
Mitchell Jones <mitchell.jones@eagles.oc.edu>
2021-11-09 15:59:18 -0600

    add perl operator code

---
+++ tutorials-point/operators/arithmetic.pl
@@ -0,0 +1,39 @@
+#!usr/bin/perl
+
+# Arithmetic operators
+
+# + (addition), adds two values
+$a = 10;
+$b = 20;
+$c = $a + $b;
+print "Value of c: $c\n"; # will display 30
+
+# - (subtraction), subtracts right hand value from left hand value
+$a = 5;
+$b = 10;
+$c = $b - $a;
+print "Value of c: $c\n"; # will display 5
+
+# * (multiplication), multiplies two values
+$a = 3;
+$b = 5;
+$c = $a * $b;
+print "Value of c: $c\n"; # will display 15
+
+# / (division), divides left hand value by right hand value
+$a = 10;
+$b = 2;
+$c = $a / $b;
+print "Value of c: $c\n"; # will display 5
+
+# % (modulus), divides left hand value by right hand value and returns remainder
+$a = 11;
+$b = 2;
+$c = $a % $b;
+print "Value of c: $c\n"; # will display 1 
+
+# ** (exponent), performs power calculation on operators
+$a = 2;
+$b = 3;
+$c = $a**$b;
+print "Value of c: $c\n"; # will display 8
